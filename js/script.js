1a394e93e840bdd9b321ee28852e57d241b745be
diamant1991 <diamant1988@mail.ru>
2015-07-01 13:30:37 +0300

    add other files

--- js/script.js
+++ js/script.js
@@ -5,3 +5,32 @@ $('input,textarea').focus(function(){
 $('input,textarea').blur(function(){
   $(this).attr('placeholder',$(this).data('placeholder'));
 });
+
+jQuery(function($){
+   $(".telephone-input").mask("+7(999)999-99-99");
+});
+
+$( document ).ready(function() {
+
+	/*$(".toggle-btn").click(function () {
+      $(".collapse-menu").slideToggle(300);
+   });*/
+
+	$('.get-call').click(function(){
+    	$('.form-mask').fadeIn(300);
+    	$('#get-call').fadeIn(300);
+    })
+
+
+    $('.form-mask,.closed-btn').click(function(){
+  	 $('.modal,.form-mask').fadeOut(300);
+  })
+
+	$(document).keyup(function(d) {
+	    if (d.keyCode == 27) {
+	        $('.form-mask').fadeOut(300);
+	        $('.modal').fadeOut(300);
+	    }
+	});
+
+});
